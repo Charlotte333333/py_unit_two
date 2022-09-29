faaae6d67f228f813ad46e8899fd160cb3c003ea
Charlotte333333 <112644693+Charlotte333333@users.noreply.github.com>
2022-09-29 10:59:22 -0400

    testing pushing

---
+++ age.py
@@ -0,0 +1,8 @@
+base_of_triangle = input("What is the base of the triangle?:")
+height_of_triangle = input("What is the height of the triangle")
+base_of_triangle = 8
+height_of_triangle = 27
+
+area_of_triangle = base_of_triangle = height_of_triangle / 2
+print("The area of a triangle with base", base_of_triangle, "and height", height_of_triangle, "is", area_of_triangle)
+
---
+++ assignment_two.py
@@ -0,0 +1,40 @@
+name = input("What is your name?")
+print("That's a beautiful name", name)
+
+you = input("How are you today?")
+if you.find("bad")!=-1:
+    print("Im sorry about that. If it makes you feel better Taylor swift is dropping a new album October 21st!!")
+else:
+    print("okay,cool. I am feeling good because Taylor is releasing a new album in late October!!")
+
+
+artist=input("Whats your fav music artist?")
+if artist.find("taylor")!=-1:
+    print("I LOVE TAYLOR")
+else:
+    print("oh...okay. You should liten to Taylor Swift! She's really good.")
+
+concert = input("If you went to a concert which concert whould it be?")
+if concert.find("taylor")!=-1:
+    print("I WAS GONNA BUY TICKETS TO HER CONCERT! But its so expensive")
+else:
+    print("okay, I hear those tickets are expensive.")
+
+cost = input("how much would those tickets cost?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
